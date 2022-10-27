734656cf1f3c56b3533fcd7f78652f29890347e7
Douglas Nhunzvi <dougiedj@gmail.com>
2022-10-27 14:19:48 +0200

    common widgets
    Signed-off-by: Douglas Nhunzvi <dougiedj@gmail.com>

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -9,6 +9,7 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
---
+++ lib/widgets/common.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+Widget _logo() {
+  return Center(
+    child: SizedBox (
+      child: Image.asset(name)
+    ),
+  );
+}
